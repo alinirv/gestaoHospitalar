65574545820c2c639d43db239a12c881c8d0a9b0
arv <alini.vicente@yahoo.com.br>
2024-09-16 09:15:55 -0300

    feat: adiciona pagina login

--- systemgh/src/components/pages/login.js
+++ systemgh/src/components/pages/login.js
@@ -1,1 +1,62 @@
-// implementa a pagina de login
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authService from '../../service/authService';
+import { Link } from 'react-router-dom'; 
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const history = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = authService.login(username, password);
+
+    if (user) {
+      history.push(`/${user.role}`);
+    } else {
+      setError('Credenciais incorretas. Tente novamente.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="username">Usuário</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            placeholder="Digite seu usuário"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Digite sua senha"
+          />
+        </div>
+        {error && <p className="error-message">{error}</p>}
+        <button type="submit" className="login-btn">Entrar</button>
+        <br/>
+        <Link to="/" >Voltar</Link>
+      </form>
+      <footer className="home-footer">
+        <p>&copy; 2024 Sistema de Gestão Hospitalar. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Login;
