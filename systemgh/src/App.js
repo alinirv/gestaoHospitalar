522a5a1d34704112ae995fbe2c6c7650718f5022
arv <alini.vicente@yahoo.com.br>
2024-09-16 08:46:55 -0300

    feat: adiciona fucnionalidade de rotas

--- systemgh/src/App.js
+++ systemgh/src/App.js
@@ -1,11 +1,16 @@
-
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 
+
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
