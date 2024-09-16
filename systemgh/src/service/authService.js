66bfc5ebcacb868e2680cf1e4b35b196ca3a19f8
arv <alini.vicente@yahoo.com.br>
2024-09-16 07:00:49 -0300

    feat: adiciona componente home

---
+++ systemgh/src/service/authService.js
@@ -0,0 +1,20 @@
+const authService = {
+    login: (username, password) => {
+     
+      const users = [
+        { username: 'admin', password: '123', role: 'admin' },
+        { username: 'doctor', password: '123', role: 'doctor' },
+        { username: 'patient', password: '123', role: 'patient' },
+      ];
+      const user = users.find(u => u.username === username && u.password === password);
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+      }
+      return user;
+    },
+    getCurrentUser: () => JSON.parse(localStorage.getItem('user')),
+    logout: () => localStorage.removeItem('user'),
+  };
+  
+  export default authService;
+  
