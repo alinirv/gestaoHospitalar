56a7b1e299d20bccad5c5a1f63d6be945715b778
arv <alini.vicente@yahoo.com.br>
2024-09-17 09:57:47 -0300

    refactor: refatora metodo logout para retornar para pagina login

--- systemgh/src/service/authService.js
+++ systemgh/src/service/authService.js
@@ -1,20 +1,22 @@
 const authService = {
-    login: (username, password) => {
-     
-      const users = [
-        { username: 'admin', password: '123', role: 'admin' },
-        { username: 'doctor', password: '123', role: 'doctor' },
-        { username: 'patient', password: '123', role: 'patient' },
-      ];
-      const user = users.find(u => u.username === username && u.password === password);
-      if (user) {
-        localStorage.setItem('user', JSON.stringify(user.role));
-      }
-      return user;
-    },
-    getCurrentUser: () => JSON.parse(localStorage.getItem('user')),
-    logout: () => localStorage.removeItem('user'),
-  };
-  
-  export default authService;
-  
+  login: (username, password) => {
+
+    const users = [
+      { username: 'admin', password: '123', role: 'admin' },
+      { username: 'doctor', password: '123', role: 'doctor' },
+      { username: 'patient', password: '123', role: 'patient' },
+    ];
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user.role));
+    }
+    return user;
+  },
+  getCurrentUser: () => JSON.parse(localStorage.getItem('user')),
+  logout: (navigate) => {
+    localStorage.removeItem('user');
+    if (navigate) navigate('/login');
+  }
+};
+
+export default authService;
