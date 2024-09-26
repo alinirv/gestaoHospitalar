8fb8ce62b575fc73b8f06afac4a90c1804d829eb
Everton Lourenco <promo.everton@gmail.com>
2024-09-25 22:07:59 -0300

    cria: alerta de consulta e exames

--- src/components/pages/Doctor.js
+++ src/components/pages/Doctor.js
@@ -1,39 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importação do hook de navegação
 import Header from '../utils/Header';
 import Footer from '../utils/Footer';
-
+import appointmentService from '../../service/AppointmentService';
 
 const Doctor = () => {
+  const [consultations, setConsultations] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+  const navigate = useNavigate(); // Hook para navegação
+
+  useEffect(() => {
+    // Carregar consultas ao montar o componente
+    const loadedAppointments = appointmentService.getAppointments();
+    setConsultations(loadedAppointments);
+
+    // Se houver consultas, exibir um aviso personalizado
+    if (loadedAppointments.length > 0) {
+      setShowNotification(true);
+    }
+  }, []);
+
+  // Função para navegar para a página de logs de consultas
+  const handleCheckAppointments = () => {
+    navigate('/appointmentLogs');
+  };
+
   return (
     <div className="home-container">
       <Header />
       <div className="home-content">
         <h1>Painel do Médico</h1>
-        <p><h3>Bem-vindo ao seu painel. Aqui você pode gerenciar suas consultas e exames.</h3></p>
+        <p>
+          <h3>Bem-vindo ao seu painel. Aqui você pode gerenciar suas consultas e exames.</h3>
+        </p>
+
+        {/* Exibir notificação personalizada se houver consultas */}
+        {showNotification && (
+          <div className="error-message">
+            <h1>Você tem {consultations.length} consulta(s) agendada(s).</h1>
+            <button 
+            className="login-btn"
+            onClick={handleCheckAppointments}>Verificar</button>
+          </div>
+        )}
+
+        <div className="login-container">
+          <h2>Solicitar Exames</h2>
+          <form action="/agendar-consulta" method="POST">
+            <div className="form-group">
+              <label htmlFor="exam">Exame</label>
+              <input type="text" id="exam" name="exam" required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="patient">Paciente</label>
+              <input type="text" id="patient" name="patient" required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="requestingDoctor">Médico Solicitante</label>
+              <input type="text" id="requestingDoctor" name="requestingDoctor" required />
+            </div>
+
+            <button type="submit" className="login-btn">Solicitar Exame</button>
+          </form>
+        </div>
       </div>
-
-      <div className="login-container">
-      <h2>Solicitar Exames</h2>
-        <form action="/agendar-consulta" method="POST">
-          <div className="form-group">
-            <label for="doctor">Exame</label>
-            <input type="text" id="doctor" name="doctor" required/>
-          </div>
-
-          <div className="form-group">
-            <label for="patient">Paciente</label>
-            <input type="text" id="patient" name="patient" required/>
-          </div>
-
-          <div className="form-group">
-            <label for="patient">Médico Solicitante</label>
-            <input type="text" id="patient" name="patient" required/>
-          </div>
-
-          <button type="submit" className="login-btn">Solicitar Exame</button>
-        </form>
-      </div>
-      
       <Footer />
     </div>
   );
--- src/components/pages/Patient.js
+++ src/components/pages/Patient.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../utils/Header';
 import Footer from '../utils/Footer';
 import { useNavigate } from 'react-router-dom';
-
+import examService from '../../service/ExamService'; // Serviço para obter os exames
 
 const Patient = () => {
+  const [exams, setExams] = useState([]); // Estado para armazenar exames
+  const [showNotification, setShowNotification] = useState(false); // Estado para controlar a exibição da notificação
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Carregar exames ao montar o componente
+    const loadedExams = examService.getExams(); // Simulação de carregamento dos exames
+    setExams(loadedExams);
+
+    // Se houver exames, exibir notificação
+    if (loadedExams.length > 0) {
+      setShowNotification(true);
+    }
+  }, []);
+
+  // Função para navegar para a página de logs de exames
+  const handleCheckExams = () => {
+    navigate('/examsLogs');
+  };
 
   return (
     <div className="home-container">
@@ -13,13 +31,22 @@ const Patient = () => {
       <div className="home-content">
         <h1>Painel do Paciente</h1>
         <p><h3>Bem-vindo ao seu painel. Aqui você pode agendar suas consultas e acompanhar seus exames.</h3></p>
-      </div>
+
+        {/* Exibir notificação personalizada se houver exames */}
+        {showNotification && (
+          <div className="error-message">
+            <h1>Você tem {exams.length} exame(s) agendado(s).</h1>
+            <button 
+              className="login-btn"
+              onClick={handleCheckExams}>Verificar</button>
+          </div>
+        )}
+      </div><br></br>
 
       <div className="admin-buttons">
-      <button className="login-btn" onClick={() => navigate('/appointmentScheduling')}>Agendar Consultas</button>
+        <button className="login-btn" onClick={() => navigate('/appointmentScheduling')}>Agendar Consultas</button>
       </div>
 
-      
       <Footer />
     </div>
   );
